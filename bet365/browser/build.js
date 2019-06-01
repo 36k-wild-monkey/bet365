678ae330e809c5dc4c1590d97b4614c0f4c2d10a
x-leopold <46029824+crud-boy@users.noreply.github.com>
2019-06-01 17:58:59 +0800

    1

--- bet365/browser/build.js
+++ bet365/browser/build.js
@@ -36,52 +36,52 @@ const deleteFolder = function(path) {
 };
 
 browserify(config.files).bundle(async (err, src) => {
-    if (err) {
-        console.log(err)
-    } else {
-        deleteFolder(config.clearDir)
-        await fs.mkdirSync(config.copyDir)
-        var appFileName = 'app.' + MD5(src) + '.js'
-        appFile = path.join(config.outDir, appFileName)
-        try {
-            await fs.writeFileSync(appFile, src)
-        } catch(e) {
-            console.log(e)
-            return
-        } finally {
-            console.log('copy files:');
-            for(var x in config.copyFiles) {
-                console.log('  ' + config.copyFiles[x])
-                try {
-                    var filename = path.join(
-                        config.copyDir,
-                        path.basename(config.copyFiles[x])
-                    )
-                    var buffer = await fs.readFileSync(config.copyFiles[x]).toString()
-                    for(var x in config.replaceFlags) {
-                        
-                        buffer = buffer.replace(config.replaceFlags[x], urljoin(config.static, appFileName))
+        if (err) {
+            console.log(err)
+        } else {
+            deleteFolder(config.clearDir)
+            await fs.mkdirSync(config.copyDir)
+            var appFileName = 'app.' + MD5(src) + '.js'
+            appFile = path.join(config.outDir, appFileName)
+            try {
+                await fs.writeFileSync(appFile, src)
+            } catch(e) {
+                console.log(e)
+                return
+            } finally {
+                console.log('copy files:');
+                for(var x in config.copyFiles) {
+                    console.log('  ' + config.copyFiles[x])
+                    try {
+                        var filename = path.join(
+                            config.copyDir,
+                            path.basename(config.copyFiles[x])
+                        )
+                        var buffer = await fs.readFileSync(config.copyFiles[x]).toString()
+                        for(var x in config.replaceFlags) {
+                            
+                            buffer = buffer.replace(config.replaceFlags[x], urljoin(config.static, appFileName))
+                        }
+                        await fs.writeFileSync(filename, buffer, {flag: 'w'})
+                    } catch(e) {
+                        console.log(e)
+                        console.log('copy file error: ' + config.copyFiles[x])
+                        return
                     }
-                    await fs.writeFileSync(filename, buffer, {flag: 'w'})
-                } catch(e) {
-                    console.log(e)
-                    console.log('copy file error: ' + config.copyFiles[x])
-                    return
                 }
-            }
 
-            console.log('copy to:')
-            for(var x in config.copyFiles) {
-                console.log('  ' + path.join(config.copyDir, config.copyFiles[x]))
+                console.log('copy to:')
+                for(var x in config.copyFiles) {
+                    console.log('  ' + path.join(config.copyDir, config.copyFiles[x]))
+                }
+                
+                console.log('\ncomplie files:');
+                for(var x in config.files) {
+                    console.log('  ' + config.files[x])
+                }
+                console.log('outDir:')
+                console.log('  ' + appFile)
             }
-            
-            console.log('\ncomplie files:');
-            for(var x in config.files) {
-                console.log('  ' + config.files[x])
-            }
-            console.log('outDir:')
-            console.log('  ' + appFile)
         }
-    }
-    
-})
+    })
+    .on("error", function (err) { console.log("Error: " + err.message); })
